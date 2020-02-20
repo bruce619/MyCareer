7cca5ac0bc08f2e588f608d4c22a66c99359aff8
bruce619 <chimuanyaibecheozor@gmail.com>
2020-02-20 10:57:50 +0100

    Update

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.core.files.storage import default_storage as storage
 
 # Choice Selection for Users Gender
 SEX_TYPE = (
@@ -32,10 +33,10 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super(Profile, self).save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+        img = Image.open(self.image.name)
 
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save(self.image.path)
+            img.save(self.image.name)
 
