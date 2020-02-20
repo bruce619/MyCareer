183f9a17d6837c44f3631d7353dc456145fa75ed
bruce619 <chimuanyaibecheozor@gmail.com>
2020-02-20 09:05:02 +0100

    Update

--- mycareer/settings.py
+++ mycareer/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # 'mydjangocareerapp.herokuapp.com'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mydjangocareerapp.herokuapp.com', '127.0.0.1:8000']
 
 PASSWORD_HASHERS = (
         'django.contrib.auth.hashers.PBKDF2PasswordHasher',
@@ -253,19 +253,19 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-# AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-# AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-# AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-# AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
-# S3_USE_SIGV4 = os.environ.get('S3_USE_SIGV4')
-#
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
-#
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#
-#
-# django_heroku.settings(locals())
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+S3_USE_SIGV4 = os.environ.get('S3_USE_SIGV4')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+django_heroku.settings(locals())
 
 
 
