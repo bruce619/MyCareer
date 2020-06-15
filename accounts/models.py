0e09436c8746e54816f56dfab65997960a624d1f
bruce619 <chimuanyaibecheozor@gmail.com>
2020-06-15 20:34:43 +0100

    Initial Commit

--- accounts/models.py
+++ accounts/models.py
@@ -113,6 +113,9 @@ class Profile(models.Model):
 
         img = Image.open(self.image)
 
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size, Image.ANTIALIAS)
